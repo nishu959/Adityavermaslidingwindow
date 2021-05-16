f40f0913060013420a581f1d08029d537857d47d
nishu959 <60173728+nishu959@users.noreply.github.com>
2021-05-16 12:21:57 +0530

    Add files via upload

---
+++ longestsubstringwithkuniquecharacters.py
@@ -0,0 +1,32 @@
+
+import sys
+
+s = input()
+k = int(input())
+
+i = 0
+j =0
+mx = -1*sys.maxsize
+mp ={}
+
+while(j<len(s)):
+  if s[j] in mp:
+    mp[s[j]] += 1
+  else:
+    mp[s[j]] = 1
+    
+  if(len(mp)<k) :
+    j += 1
+  elif len(mp)==k:
+    mx= max(mx, j-i+1)
+    j+=1
+  elif len(mp)>k:
+    while(len(mp)>k):
+      mp[s[i]] -= 1
+      if mp[s[i]]==0:
+        mp.pop(s[i]) 
+      i+=1
+    j+=1
+    
+        
+print(mx)
